f4126540e53d567df0dd612ef306b35a56a406f8
AhmedAlfahal <ahmednader02@gmail.com>
2023-09-13 16:23:22 -0400

    8th-1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,37 +10,36 @@ void jack_bauer(void)
 	int	i = 0;
 	int	j = 0;
 
-	while (j <= 24)
+	while (j < 24)
 	{
-		if (j <= 9)
+		i = 0;
+		while (i < 60)
 		{
-			_putchar(48);
-			_putchar(j + 48);
-			_putchar(':');
+			if (j <= 9)
+			{
+				_putchar(48);
+				_putchar(j + 48);
+				_putchar(':');
+			}
+			else if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
+				_putchar(':');
+			}
+			if (i <= 9)
+			{
+				_putchar(48);
+				_putchar(i + 48);
+			}
+			else if (i > 9)
+			{
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+			}
+			_putchar('\n');
+			i++;
 		}
-		else if (j > 9)
-		{
-			_putchar(':');
-			_putchar((j / 10) + 48);
-			_putchar((j % 10) + 48);
-		}
-		if (i <= 9)
-		{
-			_putchar(48);
-			_putchar(i + 48);
-		}
-		else if (i > 9)
-		{
-			_putchar((i / 10) + 48);
-			_putchar((i % 10) + 48);
-		}
-		_putchar('\n');
-		sleep(60);
-		i++;
-		if (i == 60)
-		{
-			i = 0;
-			j++;
-		}
+		j++;
 	}
 }

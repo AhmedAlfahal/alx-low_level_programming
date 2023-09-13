2e4ba47c0de29bdf93106d67441db529922e49d7
AhmedAlfahal <ahmednader02@gmail.com>
2023-09-13 15:55:56 -0400

    2th

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,18 @@
 void print_alphabet_x10(void)
 {
 	int	i = 0;
+	char	a = 'a';
 
 	while (i < 10)
 	{
-		print_alphabet();
+		a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
 		i++;
 	}
 }

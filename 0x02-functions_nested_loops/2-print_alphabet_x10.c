db82e772262d8a56d115129d465f6f46f6c8559e
AhmedAlfahal <ahmednader02@gmail.com>
2023-09-13 15:53:19 -0400

    1th

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - prints lower alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int	i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}

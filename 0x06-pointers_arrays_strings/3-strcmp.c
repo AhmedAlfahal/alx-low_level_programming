dbeb99c578cd9fb138062fc568874cd0a50dadaa
AhmedAlfahal <ahmednader02@gmail.com>
2023-09-22 10:36:30 -0400

    3th-1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*	_strncpy - compare s1 and s2
+*	_strcmp - compare s1 and s2
 *
 *	@s1:  char *
 *
@@ -14,7 +14,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int	i = 0;
+	int	i = 0;
 
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;

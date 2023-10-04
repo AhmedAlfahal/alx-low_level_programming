78e2e1291b3f121f109bee95e5503918f349459c
Debian <f7l@f7l.b1hmeu5crbju1k5jg1hj2sns1h.dxbx.internal.cloudapp.net>
2023-10-04 18:40:06 +0000

    0th-1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,10 @@
 *	Return: the pointer to the allocated or NULL
 *
 **/
+
 char	*create_array(unsigned int size, char c)
 {
-	int	i = 0;
+	unsigned int	i = 0;
 	char *new = NULL;
 
 	if (size <= 0)
@@ -20,5 +21,6 @@ char	*create_array(unsigned int size, char c)
 	new = malloc(sizeof(char) * (size + 1));
 	while (i < size)
 		new[i++] = c;
-	new[i] = NULL;
+	new[i] = 0;
+	return (new);
 }

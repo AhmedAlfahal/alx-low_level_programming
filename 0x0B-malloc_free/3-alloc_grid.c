8f7d65ca8316efc1fc3aa4a9e65dc0c603cfd516
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-04 19:27:25 +0000

    3th

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+*	_memset - check the code
+*
+*	@s: string to set
+*
+*	@b: character to fill the memory with
+*
+*	@n: length to be copied
+*
+*	Return: starting of the string
+*
+**/
+
+void	_memset(void *s, char b, unsigned int n)
+{
+	unsigned char	a;
+	size_t			i;
+
+	i = 0;
+	a = (unsigned char)b;
+	while (i < n)
+	{
+		((char *)s)[i] = a;
+		i++;
+	}
+}
+
+/**
+*	alloc_grid - allocate grid
+*
+*	@width: int
+*
+*	@height: int
+*
+*	Return: the pointer to the string or NULL
+*
+**/
+
+int	**alloc_grid(int width, int height)
+{
+	int **new = NULL;
+	int i = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	new = malloc(sizeof(int **) * height);
+	if (!new)
+		return (NULL);
+	while (i < height)
+	{
+		new[i] = malloc(sizeof(int *) * width);
+		if (!new[i])
+			return (NULL);
+		_memset(new[i], 0, width);
+		i++;
+	}
+	return (new);
+}

6773eb236eb726b5ce73621aea2ad26009c48ed2
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-05 11:23:07 -0400

    1th

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*	main - check the code for ALX School students.
+*
+*	@ac: int
+*
+*	@av: char **
+*
+*	Return: Always 0.
+*/
+
+int main(int ac, char **av)
+{
+	(void)av;
+	printf("%d\n", ac - 1);
+	return (0);
+}

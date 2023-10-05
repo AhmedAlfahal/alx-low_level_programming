b71274305a5c8466fb0d7c80a2b263bd5eeba74f
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-05 11:28:37 -0400

    3th

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*	main - check the code for ALX School students.
+*
+*	@ac: int
+*
+*	@av: char **
+*
+*	Return: Always 0.
+*/
+
+int main(int ac, char **av)
+{
+	if (ac == 3)
+		printf("%d\n", atoi(av[1]) * atoi(av[2]));
+	else
+	 	printf("Error\n");
+	return (0);
+}

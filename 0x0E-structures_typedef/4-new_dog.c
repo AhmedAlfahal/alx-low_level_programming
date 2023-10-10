70e2f129090ab183db2ff6288d1b684479915bc9
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-10 13:41:38 -0400

    4th-1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,53 @@
 #include "dog.h"
+#include <strings.h>
+
+/**
+*	_strlen - fins length of string
+*
+*	@s: a pointer to string
+*
+*	Return: string length
+**/
+int	_strlen(char *s)
+{
+	int	l;
+
+	if (!s)
+		return (0);
+	l = 0;
+	while (s[l] != 0)
+		l++;
+	return (l);
+}
+
+/**
+*	_strdup - duplicate a string
+*
+*	@str: char *
+*
+*	Return: the pointer to the string or NULL
+*
+**/
+
+char	*_strdup(char *str)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	dup = (char *) malloc(sizeof(char) * _strlen(str) + 1);
+	if (dup == NULL)
+		return (0);
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
 
 /**
 *	new_dog - initilizing a dog
@@ -16,12 +65,15 @@ dog_t	*new_dog(char *name, float age, char *owner)
 {
 	dog_t	*tmp = malloc(sizeof(dog_t) * 1);
 
-	if (!name)
-		return (free(tmp), NULL);
-	tmp->name = name;
+	if (!tmp)
+		return (NULL);
+	bzero(tmp, sizeof(dog_t));
+	tmp->name = _strdup(name);
+	if (!tmp->name)
+		return (free(tmp->name), free(tmp), NULL);
 	tmp->age = age;
-	if (!owner)
-		return (free(tmp), NULL);
-	tmp->owner = owner;
+	tmp->owner = _strdup(owner);
+	if (!tmp->owner)
+		return (free(tmp->owner), free(tmp), NULL);
 	return (tmp);
 }

538e2b81320ecc4c592e4ed9b6b79628ed7e4772
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-11 16:28:28 -0400

    0th

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+*	print_name - prints @name using funcution giving as pointer
+*
+*	@name: char *
+*
+*	@f: funcution pointer that takes char *
+*
+*  Return: void
+**/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+	f(name);
+}

33447be51494f2a72e9cfaceb560c360a8cea187
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-23 13:25:26 -0400

    0th-2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,6 +23,8 @@ typedef struct listint_s
 	struct listint_s	*next;
 }					listint_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
+size_t	print_listint(const listint_t *h);
+size_t	listint_len(const listint_t *h);
+void	_putchar(char c);
+
 #endif

3eb88550ca4de268740bdf3489b344205961471f
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-24 16:49:15 -0400

    9th

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head || !(*head))
 		return (NULL);
 	h = (*head);
+	tmp = h;
 	while (h && i < idx - 1)
 	{
 		h = h->next;
